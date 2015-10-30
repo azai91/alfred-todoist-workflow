2d3c45e561a5bb1490cc1d3d6e69f7850d356678
Alex Zai <azai91@gmail.com>
2015-10-29 17:56:01 -0700

    (feat) successfully posts

---
+++ src/todoist.py
@@ -0,0 +1,34 @@
+from workflow import Workflow, PasswordNotFound, ICON_TRASH, ICON_WARNING, ICON_USER
+import requests
+from config import API_KEY
+
+UPDATE_SETTINGS = {'github_slug' : 'azai91/alfred-drive-workflow'}
+HELP_URL = 'https://github.com/azai91/alfred-drive-workflow/issues'
+
+wf = Workflow(update_settings=UPDATE_SETTINGS, help_url=HELP_URL)
+
+def main(_):
+  user_input = wf.args[0]
+
+  if wf.update_available:
+    wf.add_item(
+      'An update is available!',
+      autocomplete='workflow:update',
+      valid=False
+    )
+
+  data = {
+    "token" : API_KEY,
+    "content" : user_input
+  }
+
+  try:
+    requests.post('https://todoist.com/API/v6/add_item',data)
+  except:
+    pass
+
+
+  return 0
+
+if __name__ == '__main__':
+  wf.run(main)
