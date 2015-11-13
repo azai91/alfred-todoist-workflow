a8a5046c7e0b05119259991f8a97b6e5e6be3fe4
Alex Zai <azai91@gmail.com>
2015-11-13 12:53:53 -0800

    (feat) add read_write scopes

--- src/config.py
+++ src/config.py
@@ -1,7 +1,8 @@
 CLIENT_ID = '959e19a2843c4afa9086c76659dd17f4'
 CLIENT_SECRET = '2c16535ef1d44511b383ed2fd2caad59'
-SCOPE = 'task:add'
+SCOPE = 'task:add,data:read_write'
 
 AUTH_URL = 'https://todoist.com/oauth/authorize?client_id=%s&scope=%s&state=%s' % (CLIENT_ID, SCOPE, 'Alfred')
 ADD_ITEM_URL = 'https://todoist.com/API/v6/add_item'
 TOKEN_URL = 'https://todoist.com/oauth/access_token'
+SYNC_URL = 'https://todoist.com/API/v6/sync'
