b3f69aa65f0580be506125eb12d84ed4b7153440
Alex Zai <azai91@gmail.com>
2015-11-13 22:50:41 -0800

    (feat) validates coordinates format

--- src/todoist.py
+++ src/todoist.py
@@ -19,36 +19,57 @@ def main(_):
       arg="login",
       autocomplete='Add Account',
       valid=True)
+
   if user_input.lower() in "Remove Account".lower():
     wf.add_item(title="Remove Account",
       arg="logout",
       autocomplete="Remove Account",
       valid=True)
+
   if user_input.lower() in "Sync Account".lower():
     wf.add_item(title="Sync Account",
       arg="sync",
       autocomplete="Sync Account",
       valid=True)
-  if user_input.lower() in "Set Home Location".lower():
+
+  if user_input.lower() in "Set Home Location ".lower():
     wf.add_item(title="Set Home Location [long,lat]",
       autocomplete="Set Home Location ")
-  if user_input.lower().startswith("Set Home Location ".lower()):
-    # insert long lat checker
-    wf.add_item(title="Set Home Location %s" % user_input[18:],
-      arg="home%s" % user_input[18:].replace(" ",""),
-      valid=True)
-  if user_input.lower() in "Set Work Location".lower():
+  elif user_input.lower().startswith("Set Home Location ".lower()):
+    coordinates = user_input[18:].replace(" ","")
+    if validate_coordinates(coordinates):
+      wf.add_item(title="Set Home Location %s" % user_input[18:],
+        arg="home%s" % coordinates,
+        valid=True)
+    else:
+      wf.add_item(title="Invalid Coordinates Format")
+
+  if user_input.lower() in "Set Work Location ".lower():
     wf.add_item(title="Set Work Location [long,lat]",
       autocomplete="Set Work Location ")
-  if user_input.lower().startswith("Set Work Location ".lower()):
-    # insert long lat checker
-    wf.add_item(title="Set Work Location %s" % user_input[18:],
-      arg="work%s" % user_input[18:].replace(" ",""),
-      valid=True)
+  elif user_input.lower().startswith("Set Work Location ".lower()):
+    coordinates = user_input[18:].replace(" ","")
+    if validate_coordinates(coordinates):
+      wf.add_item(title="Set Work Location %s" % user_input[18:],
+        arg="work%s" % coordinates,
+        valid=True)
+    else:
+      wf.add_item(title="Invalid Coordinates Format")
 
   wf.send_feedback()
   return 0
-33.979758, -118.422545
+
+def validate_coordinates(coordinates):
+  long = None
+  lat = None
+  if ';' in coordinates:
+    coordinates = coordinates.split(';')
+    [long,lat] = coordinates
+  else:
+    long = coordinates
+    lat = 0
+
+  return isinstance(long, int) and isinstance(lat, int)
 
 if __name__ == '__main__':
   wf.run(main)
